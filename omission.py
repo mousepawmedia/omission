73a5be5203fb9d533dd9cf10bc381499381c3708
Jason C. McDonald <jcmcdonald@mousepawgames.com>
2017-08-05 13:36:11 -0700

    Snapcraft building (not finished)

--- omission.py
+++ omission.py
@@ -1,2 +1,7 @@
+#!/usr/bin/env python
+"""
+Runs the Omission package.
+"""
+
 from omission import run
 run.run()
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Omission',
+      version='1.0',
+      description='A deceptively simple word puzzle.',
+      author='MousePaw Games',
+      author_email='info@mousepawgames.com',
+      url='https://www.mousepawgames.com/omission',
+      license='BSD-3',
+      packages=['omission',
+                'omission.data',
+                'omission.game',
+                'omission.interface'],
+      entry_points={'gui_scripts': ['omission = omission.__main__:main']},
+      install_requires=[
+          'cython == 0.25.2',
+          'kivy >= 1.10.0',
+          'appdirs >= 1.4.3'
+          ],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3',
+          'Topic :: Games/Entertainment :: Puzzle Games'
+          ]
+     )
